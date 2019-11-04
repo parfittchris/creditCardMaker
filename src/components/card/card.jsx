5bda9a36cc1794b3b16f078bcc825c76a25a7fcc
Chris Parfitt <parfitt.chris7@gmail.com>
2019-11-03 21:59:33 -0500

    initial commit

---
+++ src/components/card/card.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import CardImg from './../../assets/card.jpeg';
+import './card.css';
+
+export default function Card(props) {
+  let {
+    cardholder,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    thirteen,
+    fourteen,
+    fifteen,
+    sixteen,
+    monthSelector,
+    yearSelector
+  } = props.info;
+
+  return (
+    <div className='cardSectionOverall'>
+      <img className='cardImg' src={CardImg} alt='' />
+      <div className='cardHolderCard'>
+        <p className='cardHolderTitle text'>Cardholder</p>
+        <p className='nameInput text'>{cardholder}</p>
+      </div>
+      <div className='numberInputCard'>
+        <div className='firstSect cardSection'>
+          <p className='oneCard text'>{one || '#'}</p>
+          <p className='twoCard text'>{two || '#'}</p>
+          <p className='threeCard text'>{three || '#'}</p>
+          <p className='fourCard text'>{four || '#'}</p>
+        </div>
+        <div className='secondCard cardSection'>
+          <p className='fiveCard text'>{five || '#'}</p>
+          <p className='sixCard text'>{six || '#'}</p>
+          <p className='sevenCard text'>{seven || '#'}</p>
+          <p className='eightCard text'>{eight || '#'}</p>
+        </div>
+        <div className='thirdCard cardSection'>
+          <p className='nineCard text'>{nine || '#'}</p>
+          <p className='tenCard text'>{ten || '#'}</p>
+          <p className='elevenCard text'>{eleven || '#'}</p>
+          <p className='twelveCard text'>{twelve || '#'}</p>
+        </div>
+        <div className='fourthCard cardSection'>
+          <p className='thirteenCard text'>{thirteen || '#'}</p>
+          <p className='fourteenCard text'>{fourteen || '#'}</p>
+          <p className='fifteenCard text'>{fifteen || '#'}</p>
+          <p className='sixteenCard text'>{sixteen || '#'}</p>
+        </div>
+      </div>
+      <div className='expirationSectionCard'>
+        <p className='expirationTitle text'>Expires</p>
+        <div className='expirationChange'>
+          <p className='expItm text'>{monthSelector || 'MM'}</p>
+          <p className='expItm text'>/</p>
+          <p className='expItm text'>{yearSelector || 'YY'}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/form/form.jsx
@@ -0,0 +1,207 @@
+import React from 'react';
+import Card from '../card/card.jsx';
+import './form.css';
+
+class CardForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+    this.numbersOnly = this.numbersOnly.bind(this);
+    this.makeActive = this.makeActive.bind(this);
+  }
+
+  componentDidMount() {
+    let months = document.getElementById('monthSelector');
+    let years = document.getElementById('yearSelector');
+
+    for (let monthNum = 1; monthNum <= 12; monthNum++) {
+      if (monthNum < 10) monthNum = '0' + monthNum.toString();
+      let option = document.createElement('option');
+      option.value = monthNum;
+      option.text = monthNum;
+      months.add(option);
+    }
+
+    for (let yearNum = 2019; yearNum <= 2030; yearNum++) {
+      let option = document.createElement('option');
+      option.value = yearNum.toString().slice(2);
+      option.text = yearNum;
+      years.add(option);
+    }
+  }
+
+  makeActive(e) {
+    let actives = Array.from(document.getElementsByClassName('active'));
+
+    if (actives.length > 0) {
+      actives.forEach(ele => {
+        ele.classList.remove('active');
+      });
+    }
+
+    let element = document.getElementsByClassName(
+      e.target.classList + 'Card'
+    )[0];
+
+    if (element) {
+      element.classList.add('active');
+    }
+  }
+
+  numbersOnly(e) {
+    let element = e.target;
+    let regex = /[^0-9]/gi;
+    element.value = element.value.replace(regex, '');
+  }
+
+  handleChange(e) {
+    let key = e.target.id;
+    let value = e.target.value;
+    let split;
+
+    switch (key) {
+      case 'firstInput':
+        split = value.split('');
+        this.setState({
+          one: split[0],
+          two: split[1],
+          three: split[2],
+          four: split[3]
+        });
+        break;
+      case 'secondInput':
+        split = value.split('');
+        this.setState({
+          five: split[0],
+          six: split[1],
+          seven: split[2],
+          eight: split[3]
+        });
+        break;
+      case 'thirdInput':
+        split = value.split('');
+        this.setState({
+          nine: split[0],
+          ten: split[1],
+          eleven: split[2],
+          twelve: split[3]
+        });
+        break;
+      case 'fourthInput':
+        split = value.split('');
+        this.setState({
+          thirteen: split[0],
+          fourteen: split[1],
+          fifteen: split[2],
+          sixteen: split[3]
+        });
+        break;
+      default:
+        this.setState({
+          [key]: value
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={this.makeActive}>
+        <Card info={this.state} />
+        <form className='cardForm' autoComplete='off'>
+          <div className='formContent'>
+            <div className='cardNumberSection'>
+              <label className='numberLabel'>Number</label>
+              <div className='cardNumbers'>
+                <input
+                  type='text'
+                  className='numberInput'
+                  name=''
+                  id='firstInput'
+                  maxlength='4'
+                  onKeyPress={this.numbersOnly}
+                  onChange={this.handleChange}
+                  onClick={this.makeActive}
+                />
+                <input
+                  type='text'
+                  className='numberInput'
+                  name=''
+                  id='secondInput'
+                  maxlength='4'
+                  onKeyPress={this.numbersOnly}
+                  onChange={this.handleChange}
+                  onClick={this.makeActive}
+                />
+                <input
+                  type='text'
+                  className='numberInput'
+                  name=''
+                  id='thirdInput'
+                  maxlength='4'
+                  onKeyPress={this.numbersOnly}
+                  onChange={this.handleChange}
+                  onClick={this.makeActive}
+                />
+                <input
+                  type='text'
+                  className='numberInput'
+                  name=''
+                  id='fourthInput'
+                  maxlength='4'
+                  onKeyPress={this.numbersOnly}
+                  onChange={this.handleChange}
+                  onClick={this.makeActive}
+                />
+              </div>
+            </div>
+            <div className='cardHolderSection'>
+              <label className='cardHolderLabel'>Name</label>
+              <input
+                type='text'
+                className='cardHolder'
+                name=''
+                id='cardholder'
+                maxlength='28'
+                onChange={this.handleChange}
+                onClick={this.makeActive}
+              />
+            </div>
+            <div className='selectorSection'>
+              <select
+                name=''
+                id='monthSelector'
+                className='expirationSection'
+                onChange={this.handleChange}
+                onClick={this.makeActive}
+              >
+                Month
+              </select>
+              <select
+                name=''
+                id='yearSelector'
+                className='expirationSection'
+                onChange={this.handleChange}
+                onClick={this.makeActive}
+              >
+                Year
+              </select>
+              <label className='cwLabel'>CW</label>
+              <input
+                type='text'
+                className='cw'
+                name=''
+                id='cw'
+                maxlength='4'
+                onChange={this.handleChange}
+                onClick={this.makeActive}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CardForm;
